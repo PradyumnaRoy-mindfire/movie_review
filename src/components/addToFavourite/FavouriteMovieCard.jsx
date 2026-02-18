1ca30239b5dd54da176eb9280c8007e04f841072
PradyumnaRoy-mindfire <pradyumnar@mindfiresolutions.com>
2026-02-18 10:02:53 +0530

    feat: refactor MovieCard and FavouriteMovieCard components to use memo for performance optimization and enhance drag-and-drop functionality in Favourite page

--- src/components/addToFavourite/FavouriteMovieCard.jsx
+++ src/components/addToFavourite/FavouriteMovieCard.jsx
@@ -1,31 +1,38 @@
+import { memo } from 'react';
 import { Grip, Calendar, Star } from 'lucide-react';
 import FavouriteButton from './FavouriteButton';
+import { Link } from 'react-router-dom';
 
 const imageBaseUrl = import.meta.env.VITE_IMAGE_BASE_URL;
 const placeHolderImageUrl = import.meta.env.VITE_PLACEHOLDER_IMAGE_URL;
 
-const FavouriteMovieCard = ({ movie, onDragStart, onDragEnd }) => {
+const FavouriteMovieCard = memo(function FavouriteMovieCard({
+  movie,
+  onDragStart,
+  onDragEnd,
+}) {
   const posterUrl = movie.poster_path
     ? `${imageBaseUrl}w200${movie.poster_path}`
     : placeHolderImageUrl;
   return (
     <>
       <div
-        className="flex items-center border-2 bg-gradient-to-r from-orange-200 to-pink-200 border-gray-300  hover:shadow-lg hover:scale-103 shadow-gray-400 rounded-lg p-2 m-4 transition-transform duration-300 cursor-move"
+        className="flex items-center border-2 bg-gradient-to-r from-orange-200 to-pink-200 border-gray-300  hover:shadow-lg hover:scale-103 shadow-gray-400 rounded-lg p-2 m-4 transition-transform duration-300 cursor-grabbing"
         draggable
         onDragStart={onDragStart}
         onDragEnd={onDragEnd}
       >
         <Grip size={18} className="inline-block mr-2 top" />
-        <img
-          src={posterUrl}
-          alt={movie.title}
-          className="w-25 h-20 sm:h-20 md:h-25 lg:h-30 object-cover rounded-md"
-        />
+        <Link to={`/movie/${movie.id}/details`}>
+          <img
+            src={posterUrl}
+            alt={movie.title}
+            className="w-25 h-20 sm:h-20 md:h-25 lg:h-30 object-cover rounded-md"
+          />
+        </Link>
         <div className="mx-2 flex flex-col items-baseline h-15 md:h-18 lg:h-20 justify-center">
           <h1 className="text-lg font-bold">{movie.title}</h1>
           <div className="flex items-center justify-center gap-5">
-            {/* <span className=''></span> */}
             <p className="text-sm text-gray-500">
               <Star size={18} className="inline-block mr-1 text-yellow-500" />
               {movie.vote_average}
@@ -43,6 +50,6 @@ const FavouriteMovieCard = ({ movie, onDragStart, onDragEnd }) => {
       </div>
     </>
   );
-};
+});
 
 export default FavouriteMovieCard;
